c8df7edd02ff875663c8c5695da51dfc911819a3
enriktigasna <42349218+enriktigasna@users.noreply.github.com>
2023-11-17 18:08:56 +0100

    Extracted timeout to top of function

--- crash.py
+++ crash.py
@@ -5,54 +5,52 @@ import hmac
 import sys
 import time
 
+# Constants
 balance = 100
+TIMEOUT = 1
+e = 2**52
+salt = "0000000000000000000fa3b65e43e4240d71762a5bf397d5304b2596d116859c"
 
 def get_hash():
     return str(random.getrandbits(128))
 
-e = 2**52
-salt = "0000000000000000000fa3b65e43e4240d71762a5bf397d5304b2596d116859c"
-
 def get_result():
-    hash = get_hash()
+    hash_value = get_hash()
 
-    hm = hmac.new(str.encode(hash), b'', hashlib.sha256)
+    hm = hmac.new(str.encode(hash_value), b'', hashlib.sha256)
     hm.update(salt.encode("utf-8"))
     h = hm.hexdigest()
-    if (int(h, 16) % 33 == 0):
+    if int(h, 16) % 33 == 0:
         return 1
     h = int(h[:13], 16)
     e = 2**52
     return (((100 * e - h) / (e-h)) // 1) / 100.0
 
-
-# Gives user one second to send an input, else it returns default value
-def readInput( caption, default, timeout = 1):
+# Gives the user one second to send an input; otherwise, it returns the default value
+def read_input(caption, default):
     start_time = time.time()
     sys.stdout.write(caption)
     sys.stdout.flush()
-    input = ''
+    user_input = ''
+    
     while True:
         if msvcrt.kbhit():
             byte_arr = msvcrt.getche()
-            if ord(byte_arr) == 13: # enter_key
+            if ord(byte_arr) == 13:  # Enter key
                 break
-            elif ord(byte_arr) >= 32: #space_char
-                input += "".join(map(chr,byte_arr))
-        if len(input) == 0 and (time.time() - start_time) > timeout:
+            elif ord(byte_arr) >= 32:  # Space character
+                user_input += "".join(map(chr, byte_arr))
+        if len(user_input) == 0 and (time.time() - start_time) > TIMEOUT:
             break
+    
+    print('')  # Needed to move to the next line
+    return user_input if len(user_input) > 0 else default
 
-    print('')  # needed to move to next line
-    if len(input) > 0:
-        return input
-    else:
-        return default
-
-def playRound(bet):
+def play_round(bet):
     crash = get_result()
 
     multiplier = 0
-    while readInput(f"Current multiplier: {multiplier}. Current return: {bet*multiplier}", "NaN") == "NaN":
+    while read_input(f"Current multiplier: {multiplier}. Current return: {bet * multiplier}", "NaN") == "NaN":
         print("PRESS SPACE THEN ENTER TO CASH OUT")
         multiplier += 0.1
         multiplier = round(multiplier, 1)
@@ -61,7 +59,7 @@ def playRound(bet):
             multiplier = 0
             print("You crashed! ")
             break
-    bet_return = round(multiplier*bet, 1)
+    bet_return = round(multiplier * bet, 1)
     print(f"You made {bet_return}")
     return bet_return
 
@@ -69,7 +67,7 @@ while True:
     to_bet = input(f"How much do you want to bet? Your current balance is {balance}. ")
     try:
         to_bet = int(to_bet)
-    except:
+    except ValueError:
         print("Not a number!")
         continue
 
@@ -77,4 +75,4 @@ while True:
         print("Insufficient Funds. Try again.")
         continue
     balance -= to_bet
-    balance += playRound(to_bet)
+    balance += play_round(to_bet)
